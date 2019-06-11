2230958c2010fd64ce412bd4b08d9bf2c3f33525
Nic <nicsphehehe@gmail.com>
2019-06-11 20:41:53 +0700

    Update Icon and Frame

--- GUI PKM/Final/FinalGUI-0_0.py
+++ GUI PKM/Final/FinalGUI-0_0.py
@@ -11,14 +11,14 @@ root.configure(background='#FFFFFF') # White Color background
 ### Button Configuration
 
 ### Upper Side
-Label(root, text="Selamat Datand di Smart Plastic Recycler").grid(row=0, column=0, sticky='we')
+Label(root, text="Selamat Datand di Smart Plastic Recycler").grid(row=1, column=0, sticky='we')
 
 ### Left Side
 
 ## Icon
 # Create an Icon bar
 icon_bar = Frame(root, height=25, background='light sea green')
-icon_bar.pack(expand='no', fill='y')
+icon_bar.grid(row=0, column=0, sticky='we')
 
 # OK Icon
 '''
@@ -30,10 +30,10 @@ OKpanel.grid(row=1, column=0, sticky='w')
 
 
 ## Button
-Button(root, text='OK').grid(row=1, column=0, sticky='w')
-Button(root, text='BACK').grid(row=2, column=0, sticky='w')
-Button(root, text='CANCEl').grid(row=3, column=0, sticky='w')
-Button(root, text='HELP').grid(row=4, column=0, sticky='w')
+Button(root, text='OK').grid(row=2, column=0, sticky='w')
+Button(root, text='BACK').grid(row=3, column=0, sticky='w')
+Button(root, text='CANCEl').grid(row=4, column=0, sticky='w')
+Button(root, text='HELP').grid(row=5, column=0, sticky='w')
 
 #### Root Execution
 root.mainloop()
