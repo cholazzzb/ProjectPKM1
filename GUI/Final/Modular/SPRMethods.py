6b73f4ffeb24207fb1738a7e99762ef3d1810ace
cholazzzb <nicsphehehe@gmail.com>
2019-07-13 01:10:39 +0700

    Update state2 ok

--- GUI/Final/Modular/SPRMethods.py
+++ GUI/Final/Modular/SPRMethods.py
@@ -6,12 +6,19 @@ def changeframe(frame, state):
         state=True
         return frame.grid()
 
-def show_label(self, event=None):
+def show_label(self):
     self.label.lift(self.frame)
 
-def hide_label(self, event=None):
-    self.destroy
+def hide_button(event):
+    event.widget.grid_remove()
     #self.label.lower(self.frame)
 
 def xprint():
     print("Hello World")
+    
+    
+    
+### Kedepannya keliatannya ini ditaro di SPRGUI.py
+#----- Event State 2
+#def s2_OK():
+    
