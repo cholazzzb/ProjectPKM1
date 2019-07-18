489557d0838e0b5dd5c5a0946554fd855004b95d
Nic <nicsphehehe@gmail.com>
2019-07-18 10:37:02 +0700

    Udah update blm ya

--- GUI/Code/stopDelay.py
+++ GUI/Code/stopDelay.py
@@ -1,19 +1,26 @@
 from tkinter import *
-root = Tk()
+import time
+import random
 
-root.title("Tick")
-root.geometry("320x400")
+tk = Tk()
+canvas = Canvas(tk, width=1920, height=1080, background="grey")
+canvas.pack()
 
-AFTER = None
-def tick():
-    print ("tick!")
-    global AFTER
-    AFTER = root.after(1000, tick)
+def xy(event):
+    xm, ym = event.x, event.y
 
-def key_pressed(event):
-    if event.keysym == "Return":
-        root.after_cancel(AFTER)
+def task():
+    w=random.randint(1,1000)
+    h=random.randint(1,1000)
+    canvas.create_rectangle(w,h,w+150,h+150)
+    def callback(event):
+        if True:
+            print("clicked2")
+            # 'solve' is used here to stop the after... methods.
+            tk.after_cancel(solve)
+    canvas.bind("<Button-1>",callback)
+    solve = tk.after(1000,task)
+# above and below tk.after is set to 'solve' a variable.
+solve = tk.after(1000,task)
 
-root.bind("<Key>", key_pressed)
-root.after(1000, tick)
-mainloop()
+tk.mainloop()
